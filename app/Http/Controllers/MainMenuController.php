07190ddce13f180b10386033361a81eef2ac05f7
mohammed.abozamel112 <mohammed.abozamel112@gmail.com>
2024-10-14 21:25:36 +0300

    new

---
+++ app/Http/Controllers/MainMenuController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MainMenu;
+use App\Http\Requests\StoreMainMenuRequest;
+use App\Http\Requests\UpdateMainMenuRequest;
+
+class MainMenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMainMenuRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MainMenu $mainMenu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MainMenu $mainMenu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMainMenuRequest $request, MainMenu $mainMenu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MainMenu $mainMenu)
+    {
+        //
+    }
+}
